3d69796bdeed285f3a25e5879f100feb9dd8065b
Sai Kumar <kumarvukkum@gmail.com>
2022-07-02 08:22:30 +0530

    latest changes

--- src/main/java/com/nt/rest/UnlockAccRestController.java
+++ src/main/java/com/nt/rest/UnlockAccRestController.java
@@ -3,6 +3,7 @@ package com.nt.rest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import com.nt.bindings.UnlockAccForm;
 import com.nt.service.IUserMgmtService;
 
 @RestController
+@CrossOrigin
 public class UnlockAccRestController {
      @Autowired    
 	 private IUserMgmtService service;
